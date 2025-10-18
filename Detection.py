aa58b1724f7ba4818a353e59b1f3346b9a3cb37b
karanVijayShankar <s4710142@student.uq.edu.au>
2025-10-18 16:45:11 +1000

    Updated Detection.py
    Updated Detection.py to feed camera feed into road detection model

--- Detection.py
+++ Detection.py
@@ -1,14 +1,16 @@
 from ultralytics import YOLO
 import cv2
 
-# Load your model
-model = YOLO("/home/amaana/Desktop/servo/venv/Models/bin_detection_best.pt")
+# Load your bin_detection_model
+bin_detection_model = YOLO("bin_detection_models/bin_detection_best.pt")
+road_detection_model = YOLO("road_detection.pt")
 
 # Open camera
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 cap.set(cv2.CAP_PROP_XI_FRAMERATE, 1)
+
 
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 middle_x = frame_width // 2  # vertical divider
@@ -18,17 +20,20 @@ while True:
     if not ret:
         break
 
-    results = model(frame, imgsz=320, verbose=False)
-    annotated = results[0].plot()
+    bin_results = bin_detection_model(frame, imgsz=320, verbose=False)
+    road_results = road_detection_model(frame, imgsz=320, verbose=False)
 
-    # Draw a dividing line in the middle
-    cv2.line(annotated, (middle_x, 0), (middle_x, frame.shape[0]), (0, 255, 255), 2)
+    annotated_bin = bin_results[0].plot()
+    annotated_road = road_results[0].plot()
 
-    # Go through detections
-    for box in results[0].boxes:
+    # BIN ONLY: Draw a dividing line in the middle
+    cv2.line(annotated_bin, (middle_x, 0), (middle_x, frame.shape[0]), (0, 255, 255), 2)
+
+    # Go through bin detections
+    for box in bin_results[0].boxes:
         x1, y1, x2, y2 = box.xyxy[0]
         cls = int(box.cls[0])
-        label = model.names[cls]
+        bin_label = bin_detection_model.names[cls]
 
         # Find object center
         center_x = (x1 + x2) / 2
@@ -39,25 +44,18 @@ while True:
         else:
             side = "right"
         
-        binlocation = f"{label}{side}"
+        binlocation = f"{bin_label} {side}"
         
         print(binlocation)
 
-        # Display side text
-        cv2.putText(
-            annotated,
-            f"{label} ({side})",
-            (int(x1), int(y1)+300),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.6,
-            (255, 255, 0),
-            2
-        )
-
-    cv2.imshow("Zone Detection", annotated)
-
-    if cv2.waitKey(1) == ord('q'):
-        break
+    # Go through road detections
+    for tile in road_results[0].boxes:
+        x1, y1, x2, y2 = box.xyxy[0]
+        cls = int(box.cls[0])
+        road_label = road_detection_model.names[cls]
+        tile_type = f"{road_label}"
+        
+        print(tile_type)
 
 cap.release()
 cv2.destroyAllWindows()
