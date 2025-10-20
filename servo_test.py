bf614278273ab02c9bb19397177a906c4ad50e21
amaana <amaana.hussain@uqconnect.edu.au>
2025-10-20 18:00:38 +1100

    set 0 position

--- servo_test.py
+++ servo_test.py
@@ -2,4 +2,9 @@
 from gpiozero import AngularServo
 from time import sleep
 
-servo = AngularServo(17, min_angle=-90, max_angle=90)
+servo = AngularServo(18, min_angle = -90, max_angle=90) #arm 
+servo.angle = 0 # mid-point 
+sleep(2)
+
+servo.angle = 90
+sleep(2)
