531dc0c86a3486ca341e839996eec3fd12215a82
pxybrandon <pxybrandon@gmail.com>
2025-10-20 20:13:26 +1000

    changed ultrasonic

--- Main.py
+++ Main.py
@@ -5,20 +5,20 @@ import MOTOR_CODE
 #colour_sensor2 = Colour_sensor.ColourSensor(channel=1)
 #colour_sensor3 = Colour_sensor.ColourSensor(channel=2)
 #colour_sensor4 = Colour_sensor.ColourSensor(channel=3)
-#u_sensor = ULTRASONIC_CODE.ObstacleDetector(trigger_pin=23, echo_pin=24)
+u_sensor = ULTRASONIC_CODE.ObstacleDetector(trigger_pin=23, echo_pin=24)
 import time
-motor_assembly = MOTOR_CODE.Motor(23, 24, 17, 27, 18, 2) # This is what we're using on the prototype
-while True:
-    time.sleep(2)
-    motor_assembly.forward()
-    time.sleep(1)
-    motor_assembly.stop()
+#motor_assembly = MOTOR_CODE.Motor(23, 24, 17, 27, 18, 2) # This is what we're using on the prototype
+# while True:
+#     time.sleep(2)
+#     motor_assembly.forward()
+#     time.sleep(1)
+#     motor_assembly.stop()
 
-#while True:
+while True:
     #colour_sensor1.readRGB()
     #colour_sensor2.readRGB()
     #colour_sensor3.readRGB()
     #colour_sensor4.readRGB()
     #time.sleep(2)
-    #u_sensor.obstacles_present()
-    #time.sleep(2)
+    u_sensor.obstacles_present()
+    time.sleep(2)
