c4859e85cab757dc1e099cd7e7103411a469e618
Tahhhhm <tahhhhm@Tahhhhms-MacBook-Pro.local>
2025-10-20 17:43:18 +1000

    testing

--- servo_test.py
+++ servo_test.py
@@ -4,9 +4,11 @@ from time import sleep
 
 min_angle = -90
 max_angle = 90
+
 servo_arm = AngularServo(18, min_angle=min_angle, max_angle=max_angle)
 
-servo_arm.angle = 90
+while True:
+    servo_arm.angle = 90
 
 
 
