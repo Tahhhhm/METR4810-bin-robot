fb135a9290bb7b3421e7b164094ddfb39ff6f893
pxybrandon <pxybrandon@gmail.com>
2025-10-20 20:23:10 +1000

    Update servo_test.py

--- servo_test.py
+++ servo_test.py
@@ -10,7 +10,7 @@ servo_arm = AngularServo(18, min_angle=min_angle, max_angle=max_angle)
 #while True:
     #for i in range(-180, 180, 10):
 servo_arm.angle = 0
-print(f"Servo angle set to: {i} degrees")
+
 sleep(0.5)
     #i =-180
     
