de9af4679854582839a87c1d9cd977cf75fa4f94
Tahhhhm <tahhhhm@Tahhhhms-MacBook-Pro.local>
2025-10-20 19:02:15 +1000

    idk anymore

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
 import Colour_sensor 
 import ULTRASONIC_CODE
-colour_sensor1 = Colour_sensor.ColourSensor(channel=0)
-colour_sensor2 = Colour_sensor.ColourSensor(channel=1)
-colour_sensor3 = Colour_sensor.ColourSensor(channel=2)
-colour_sensor4 = Colour_sensor.ColourSensor(channel=3)
+#colour_sensor1 = Colour_sensor.ColourSensor(channel=0)
+#colour_sensor2 = Colour_sensor.ColourSensor(channel=1)
+#colour_sensor3 = Colour_sensor.ColourSensor(channel=2)
+#colour_sensor4 = Colour_sensor.ColourSensor(channel=3)
 u_sensor = ULTRASONIC_CODE.ObstacleDetector(trigger_pin=23, echo_pin=24)
 import time
 
