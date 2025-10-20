045e3899a633425204e896f7dea0bd5166fe6585
Tahhhhm <tahhhhm@Tahhhhms-MacBook-Pro.local>
2025-10-20 16:54:14 +1000

    ajda

--- Main.py
+++ Main.py
@@ -6,12 +6,8 @@ colour_sensor4 = Colour_sensor.ColourSensor(channel=3)
 import time
 
 while True:
-    data1 = colour_sensor1.readRGB()
-    data2 = colour_sensor2.readRGB()
-    data3 = colour_sensor3.readRGB()
-    data4 = colour_sensor4.readRGB()
-    print(data1)
-    print(data2)
-    print(data3)
-    print(data4)
+    colour_sensor1.readRGB()
+    colour_sensor2.readRGB()
+    colour_sensor3.readRGB()
+    colour_sensor4.readRGB()
     time.sleep(2)
--- servo_test.py
+++ servo_test.py
@@ -7,8 +7,8 @@ max_angle = 90
 servo_arm = AngularServo(18, min_angle=min_angle, max_angle=max_angle)
 
 servo_arm.angle = 0
+print("Servo set to 0 degrees")
 sleep(2)
-
 
 
 
