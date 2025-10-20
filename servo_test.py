ca4c636f4076d007c27ab1d6a95c99ac71253c07
Tahhhhm <tahhhhm@Tahhhhms-MacBook-Pro.local>
2025-10-20 18:54:35 +1000

    idk anymore

--- servo_test.py
+++ servo_test.py
@@ -2,13 +2,13 @@
 from gpiozero import AngularServo
 from time import sleep
 
-min_angle = -90
-max_angle = 90
+min_angle = -180
+max_angle = 180
 
 servo_arm = AngularServo(18, min_angle=min_angle, max_angle=max_angle)
 
 while True:
-    for i in range(-90, 90, 10):
+    for i in range(-180, 180, 10):
         servo_arm.angle = i
         print(f"Servo angle set to: {i} degrees")
         sleep(0.5)
