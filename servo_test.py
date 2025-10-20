29cef18017d787c867a20e1815e7947d6522b8a2
pxybrandon <pxybrandon@gmail.com>
2025-10-20 20:47:05 +1000

    Update servo_test.py

--- servo_test.py
+++ servo_test.py
@@ -4,8 +4,11 @@ from time import sleep
 servo_arm = AngularServo(18, min_angle=-90, max_angle=90)
 
 servo_arm.angle = -90
-sleep(2)
-
+sleep(3)
+servo_arm.angle = 0
+sleep(3)
+servo_arm.angle = 90
+sleep(3)
 
 
 
