eabcd84fe5de4688587d56e41c101e636f7d99ef
pxybrandon <pxybrandon@gmail.com>
2025-10-20 20:57:51 +1000

    Update servo_test.py

--- servo_test.py
+++ servo_test.py
@@ -1,15 +1,17 @@
-from gpiozero import AngularServo
+from gpiozero import AngularServo, Device
+from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
 
-servo_arm = AngularServo(18, min_angle=-90, max_angle=90)
+# Create a factory for pigpio (just for the servo)
+servo_factory = PiGPIOFactory()
 
+# Use pigpio factory only for this servo
+servo_arm = AngularServo(18, min_angle=-90, max_angle=90, pin_factory=servo_factory)
+
+# Move the servo
 servo_arm.angle = -90
 sleep(3)
 servo_arm.angle = 0
 sleep(3)
 servo_arm.angle = 90
 sleep(3)
-
-
-
-
