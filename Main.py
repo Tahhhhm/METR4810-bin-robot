f146419512d5c167e3848d3bbde6f446ffdc7ae8
Tahhhhm <tahhhhm@Tahhhhms-MacBook-Pro.local>
2025-10-20 16:52:04 +1000

    edit

--- Main.py
+++ Main.py
@@ -6,10 +6,10 @@ colour_sensor4 = Colour_sensor.ColourSensor(channel=3)
 import time
 
 while True:
-    data1 = colour_sensor1.readRGB(channel=0)
-    data2 = colour_sensor2.readRGB(channel=1)
-    data3 = colour_sensor3.readRGB(channel=2)
-    data4 = colour_sensor4.readRGB(channel=3)
+    data1 = colour_sensor1.readRGB()
+    data2 = colour_sensor2.readRGB()
+    data3 = colour_sensor3.readRGB()
+    data4 = colour_sensor4.readRGB()
     print(data1)
     print(data2)
     print(data3)
