30dd53f5feb8ef597a9746ecda1ffe28c2741d1d
pxybrandon <pxybrandon@gmail.com>
2025-10-21 21:49:55 +1000

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,7 +2,7 @@ from PiicoDev_Servo import PiicoDev_Servo, PiicoDev_Servo_Driver
 from PiicoDev_Unified import sleep_ms
 
 controller = PiicoDev_Servo_Driver()
-servo = PiicoDev_Servo(controller, 4, midpoint_us=1500, range_us=1800)
+servo = PiicoDev_Servo(controller, 1, midpoint_us=1500, range_us=1800)
 
 print("Testing servo angles...")
 
