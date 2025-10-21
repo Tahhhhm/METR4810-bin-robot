9ca22a174c8f80e57954daf0e8fe80f8fcdd7f7d
pxybrandon <pxybrandon@gmail.com>
2025-10-21 20:44:35 +1000

    h

--- servo_test2.py
+++
@@ -1,9 +0,0 @@
-from gpiozero import AngularServo
-from time import sleep
-
-
-
-servo_arm = AngularServo(18, min_angle= 0, max_angle= 180)
-
-servo_arm.angle = 0
-sleep(2) #time to reach the position 
