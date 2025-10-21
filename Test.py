a4f0820c9970cf3c1cb88bcb6d18925bd66d83f6
pxybrandon <pxybrandon@gmail.com>
2025-10-22 01:43:51 +1000

    Update Test.py

--- Test.py
+++ Test.py
@@ -5,14 +5,14 @@ from PiicoDev_Servo import PiicoDev_Servo, PiicoDev_Servo_Driver
 controller = PiicoDev_Servo_Driver()
 
 # Create servo object on channel 4
-servo4 = PiicoDev_Servo(controller, 4, midpoint_us=1500, range_us=1800)
+servo4 = PiicoDev_Servo(controller, 1, midpoint_us=1500, range_us=1800)
 
 print("Starting autonomous servo motion...")
 
 try:
     # Move forward slowly
     print("Moving forward...")
-    servo4.speed = 0.3
+    servo4.speed = 0.1
     sleep_ms(1000)
     print("stop")
     servo4.speed = 0
