016ef1130f38ae60ddd8592618217b271e8ea6f8
pxybrandon <pxybrandon@gmail.com>
2025-10-21 22:57:37 +1000

    Update Test.py

--- Test.py
+++ Test.py
@@ -12,8 +12,6 @@ print("Starting autonomous servo motion...")
 try:
     # Move forward slowly
     print("Moving forward...")
-    servo4.speed = 0.1
-    sleep_ms(1000)
     servo4.speed = 0
     sleep_ms(10000)
 
