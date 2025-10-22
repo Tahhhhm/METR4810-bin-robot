b07cda855d801f4e83931c08f1789adc6b4ebe2a
pxybrandon <pxybrandon@gmail.com>
2025-10-22 18:49:51 +1000

    w

--- ULTRASONIC_CODE.py
+++ ULTRASONIC_CODE.py
@@ -6,10 +6,5 @@ class ObstacleDetector:
         self.sensor = DistanceSensor(echo=echo_pin, trigger=trigger_pin)
 
     def obstacles_present(self):
-        try:
-            while True:
-                distance = self.sensor.distance * 100  # convert to cm
-                print(distance)
-        except KeyboardInterrupt:
-            print("Stopped")
-            return None
+            distance = self.sensor.distance * 100  # convert to cm
+            return distance
