9761f6d6228f25891528d21adf8cd357eb95e4bd
tahhhhm <your.email@example.com>
2025-10-23 14:56:02 +1000

    servo testing

--- servo_test1.py
+++ servo_test1.py
@@ -1,3 +1,9 @@
 from PiicoDev_Servo import PiicoDev_Servo, PiicoDev_Servo_Driver
 from PiicoDev_Unified import sleep_ms
 
+controller = PiicoDev_Servo_Driver()
+servo4 = PiicoDev_Servo(controller, 4, midpoint_us=1500, range_us = 1800)
+
+servo4.speed = -0.1
+sleep_ms(3000) 
+servo4.speed = 0
