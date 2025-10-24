a586d1afeeae2c7f16f1442a15f6fa1bf9442e68
karanVijayShankar <s4710142@student.uq.edu.au>
2025-10-24 17:25:30 +1000

    Added i2c init to servo testing code

--- servo_testworking.py
+++ servo_testworking.py
@@ -1,48 +1,75 @@
 from PiicoDev_Unified import sleep_ms
 from PiicoDev_Servo import PiicoDev_Servo, PiicoDev_Servo_Driver
 from gpiozero import Button
+import smbus2 as smbus
+MUX_ADDR = 0x70
+bus = smbus.SMBus(1)    
 from signal import pause
+from time import sleep
 
-#initialise servo driver 
+bus.write_byte(MUX_ADDR, 1<<3)
 controller = PiicoDev_Servo_Driver()
+
+servo_claw = PiicoDev_Servo(controller, 2, degrees=180)
+servo_arm = PiicoDev_Servo(controller, 3, midpoint_us=1500, range_us=1800)
+servo_base = PiicoDev_Servo(controller, 4, midpoint_us=1500, range_us=1800)
+
 micro_0 = Button(26, pull_up=True)
-micro_1 = Button(16, pull_up=True)
+micro_1 = Button(22, pull_up=True)
 
 def on_press_rest():
     # Just stops
     print("Lower Limit Switch hit...")
-    servo4.speed = 0
+    servo_base.speed = 0
     
 def on_press_dump():
     # Stops then reverses
     print("Upper Limit Switch hit")
-    servo4.speed = 0
+
+    servo_base.speed = 0
     sleep_ms(500)
-    servo4.speed = -0.3
+    turn90()
+    release()
 
 def on_release():
     print("Switch Released")
+
+def grab():
+    servo_claw.angle = 90
+    sleep(2)
+
+def release():
+    servo_claw.angle = 0
+    sleep(2)
+
+def turn180():
+    servo_base.speed = 0.1
+    sleep(7)
+    servo_base.speed = 0
+
+def turnAnti180():
+    servo_base.speed = -0.1
+    sleep(7)
+    servo_base.speed = 0
+
+def turn90():
+    servo_base.speed = 0.1
+    sleep(3.5)
+    servo_base.speed = 0
+
+def turnAnti90():
+    servo_base.speed = -0.1
+    sleep(3.5)
+    servo_base.speed = 0
 
 micro_0.when_pressed = on_press_rest
 micro_0.when_released = on_release
 micro_1.when_pressed = on_press_dump
 micro_1.when_released = on_release
 
-
-# servo1 = PiicoDev_Servo(controller, 1, degrees=180)
-#servo2 = PiicoDev_Servo(controller, 2, degrees=180)
-# servo3 = PiicoDev_Servo(controller, 3, degrees=180)
-servo4 = PiicoDev_Servo(controller, 4, midpoint_us=1500, range_us = 1800)
-
-servo4.speed = -0.1
-sleep_ms(3000)
-print("DUMP")
-servo4.speed = 0                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
-
-# Hit limit switch
-# servo4.speed = -0.5 
-# print("RETURN")
+release()
+sleep(3)
+grab()
+servo_arm.speed = 0.1
 
 
-# Hit limit switch
-pause()
