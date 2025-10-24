5e4837d4c9960b1982f95dc3a1549fdd2afdc0ba
karanVijayShankar <s4710142@student.uq.edu.au>
2025-10-24 23:15:50 +1000

    Update ColourPickUp.py

--- ColourPickUp.py
+++ ColourPickUp.py
@@ -3,7 +3,10 @@ from Colour_sensor import ColourSensor
 from PiicoDev_Unified import sleep_ms
 
 # Create the colour sensor instance
-sensor = ColourSensor(channel=0)
+sensor1 = ColourSensor(channel=0)
+sensor2 = ColourSensor(channel=1)
+sensor3 = ColourSensor(channel=2)
+sensor4 = ColourSensor(channel=3)
 
 # Threshold for green detection
 GREEN_THRESHOLD = 400 
@@ -11,15 +14,21 @@ GREEN_THRESHOLD = 400
 print("System running — waiting for green detection...")
 
 while True:
-    rgb = sensor.readRGB()             # returns a dict like {'red': 123, 'green': 456, 'blue': 789}
-    green_value = rgb['green']
-    print(f"Green: {green_value}")
+    print(sensor1.readRGB)
+    print(sensor2.readRGB)
+    print(sensor3.readRGB)
+    print(sensor4.readRGB)
+    sleep_ms(1000)
+    
+    #            # returns a dict like {'red': 123, 'green': 456, 'blue': 789}
+    # green_value = rgb['green']
+    # print(f"Green: {green_value}")
 
-    if green_value >= GREEN_THRESHOLD:
-        print("Green detected! Activating disposal sequence...")
-        #dispose()
-        sleep_ms(2000)
-    else:
-        print("No green detected.")
+    # if green_value >= GREEN_THRESHOLD:
+    #     print("Green detected! Activating disposal sequence...")
+    #     #dispose()
+    #     sleep_ms(2000)
+    # else:
+    #     print("No green detected.")
 
-    sleep_ms(10)  # adjust frequency of checks
+    # sleep_ms(10)  # adjust frequency of checks
