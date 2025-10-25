064d2a675e7814c13f514714d712e24ccfda7325
karanVijayShankar <s4710142@student.uq.edu.au>
2025-10-25 12:23:32 +1000

    Update ColourPickUp.py

--- ColourPickUp.py
+++ ColourPickUp.py
@@ -3,32 +3,33 @@ from Colour_sensor import ColourSensor
 from PiicoDev_Unified import sleep_ms
 
 # Create the colour sensor instance
-sensor1 = ColourSensor(channel=4)
-sensor2 = ColourSensor(channel=1)
-sensor3 = ColourSensor(channel=2)
-sensor4 = ColourSensor(channel=3)
+FrontL = ColourSensor(channel=1)
+FrontR = ColourSensor(channel=3)
+#sensor3 = ColourSensor(channel=2)
+#sensor4 = ColourSensor(channel=3)
 
 # Threshold for green detection
-GREEN_THRESHOLD = 400 
+GREEN_THRESHOLD = 400
+ROAD_THRESHOLD = 240 
 
 print("System running — waiting for green detection...")
 
 while True:
-    print(sensor1.readRGB())
-    print(sensor2.readRGB())
-    print(sensor3.readRGB())
-    print(sensor4.readRGB())
+    print(FrontL.readRGB())
+    print(FrontR.readRGB())
     sleep_ms(1000)
     
     #            # returns a dict like {'red': 123, 'green': 456, 'blue': 789}
-    # green_value = rgb['green']
-    # print(f"Green: {green_value}")
 
-    # if green_value >= GREEN_THRESHOLD:
-    #     print("Green detected! Activating disposal sequence...")
-    #     #dispose()
-    #     sleep_ms(2000)
-    # else:
-    #     print("No green detected.")
+    off_road_left = FrontL.readRGB()['green'] > GREEN_THRESHOLD 
+    off_road_right = FrontL.readRGB()['green'] > GREEN_THRESHOLD
+    if off_road_right:
+        print("need to turn left")
+    elif off_road_left:
+        print("need to turn right")
+    elif off_road_right and off_road_left:
+        print("stopping...")
+    else:
+        print("onward")
 
-    # sleep_ms(10)  # adjust frequency of checks
+    sleep_ms(1000)  # adjust frequency of checks
