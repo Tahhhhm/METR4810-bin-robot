047bd9b557b92059e10d8d6b7741fcf46fa5e7a8
pxybrandon <pxybrandon@gmail.com>
2025-10-25 15:58:18 +1000

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -117,7 +117,7 @@ def idle_mode():
 def start_mode():
     global bin_aligned, next_road, tile_action_paused
     print("[START MODE] Running...")
-    motor_assembly.set_speed(35)
+    motor_assembly.set_speed(45)
 
     while True:
         if switch_requested or not program_running:
