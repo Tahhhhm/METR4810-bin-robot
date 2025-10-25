d634195c537e6bc3c985f6f049ccc1288061465f
pxybrandon <pxybrandon@gmail.com>
2025-10-25 15:36:24 +1000

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -55,8 +55,8 @@ def sensor_listener():
         #distance = ultrasonic.obstacle_distance()
 
         # Off-road detection
-        off_road_left = left_road_csensor['green'] > LEFT_ROAD_THRESHOLD 
-        off_road_right = right_road_csensor['green'] > RIGHT_ROAD_THRESHOLD
+        off_road_left = left_road_csensor['green'] < LEFT_ROAD_THRESHOLD 
+        off_road_right = right_road_csensor['green'] < RIGHT_ROAD_THRESHOLD
 
         # Tile end detection
         # Tile_end = left_road_csensor > ENDING and right_road_csensor > ENDING
