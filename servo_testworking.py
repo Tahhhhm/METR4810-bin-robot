a964901bca705ccbb304c3e45c5c2d82d1314537
karanVijayShankar <s4710142@student.uq.edu.au>
2025-10-26 17:51:18 +1000

    Update servo_testworking.py

--- servo_testworking.py
+++ servo_testworking.py
@@ -54,7 +54,7 @@ micro_1.when_released = on_release
 
 try:
     servo_base.speed = 0.1
-    sleep_ms(333)
+    sleep_ms(1500)
     servo_base.speed = 0
     #servo_arm.speed = 0.1
     #sleep_ms(1000) 
