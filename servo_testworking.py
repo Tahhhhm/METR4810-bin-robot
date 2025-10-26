897bf0ab5088508c9cbea9c7fd67471ba068dcbc
karanVijayShankar <s4710142@student.uq.edu.au>
2025-10-26 19:07:31 +1000

    Update servo_testworking.py

--- servo_testworking.py
+++ servo_testworking.py
@@ -87,8 +87,8 @@ try:
     sleep_ms(900)
     servo_arm.speed = 0
     sleep(1)
-    servo_base.speed = 0.6
-    sleep_ms(500)
+    servo_base.speed = -0.6
+    sleep_ms(900)
     servo_base.speed = 0
     sleep(1)
     servo_arm.speed = -0.3
